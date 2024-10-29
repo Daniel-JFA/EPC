c451b0225501c5f91a72bffc53b4d0d1d385da3b
jjuly642 <jjuly642@gmail.com>
2024-10-29 15:24:57 -0500

    Second Commit - Validations of the login corrected

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
-import { FormsModule, FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl, AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
@@ -14,43 +14,18 @@ import { CommonModule } from '@angular/common';
 })
 
 export class LoginComponent {
-  loginForm: FormGroup;
   username: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {
-    this.loginForm = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      pass: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  constructor(private authService: AuthService, private router: Router) { }
+  login() {
+    this.authService.login(this.username, this.password).subscribe((response: any) => {
+      console.log('Respuesta del servidor:', response);
+      localStorage.setItem('token', response.token);
+      this.router.navigate(['/home']);
+    }, (error) => {
+      console.error('Error de login:', error);
     });
-  }
-
-  // login() {
-  //   this.authService.login(this.username, this.password).subscribe((response: any) => {
-  //     localStorage.setItem('token', response.token);
-  //     this.router.navigate(['/protected']);
-  //   }, (error) => {
-  //     console.error('Error de login:', error);
-  //   });
-  // }
-
-  onSubmit() {
-    if (this.loginForm.valid) {
-      const { name, pass } = this.loginForm.value;
-
-      this.authService.login(name, pass).subscribe(
-        (response: any) => {
-          localStorage.setItem('token', response.token);
-          this.router.navigate(['/home']);
-        },
-        (error) => {
-          console.error('Error de login:', error);
-          alert('Error de login. Verifica tus credenciales.');
-        }
-      );
-    } else {
-      console.log("Formulario inválido");
-    }
   }
 }
 
